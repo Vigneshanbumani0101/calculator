469f5f87d83b30a02a656cd32e24771881a99a2c
Vigneshanbumani0101 <vignesh.anbumani@ideas2it.com>
2021-11-22 23:15:18 +0530

    calculator application

---
+++ calculator.js
@@ -0,0 +1,16 @@
+Array.from(document.getElementsByClassName("value")).forEach(button => {
+    button.addEventListener('click', () => {
+        let result = document.getElementById("expression")
+        if (button.value == '=') {
+            try {
+                result.value = eval(document.getElementById("expression").value);
+            } catch {
+                result.value = "error";
+            }
+        } else if (button.value == 'clear') {
+            result.value = "";
+        } else {
+            result.value += button.value;
+        }
+    })
+})
